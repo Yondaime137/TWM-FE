b121ea6bb72f65611f8f3b88c3d9cbcf828aff7c
Yondaime137 <124598486+Yondaime137@users.noreply.github.com>
2023-03-02 14:23:39 +0100

    Twm Electro init working blank

---
+++ TwMElectro/capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'TwMElectro',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
